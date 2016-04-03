87b8f0a11143d617582d93ee43f5f6aa3a3870ec
blackendstudios <jeroflat@gmail.com>
2016-04-02 22:13:17 -0500

    style(app.js): format self-invoking arrow function

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ export default require('angular')
   .module('lazyApp', [
     require('angular-ui-router'),
     // @TODO: It's a hack! https://github.com/ocombe/ocLazyLoad/issues/179
-    (() => { require('oclazyload'); return 'oc.lazyLoad' })(),
+    (() => {
+       require('oclazyload');
+       return 'oc.lazyLoad'
+    })(),
     /*
       uncomment to move msg-store to bundle.js only instead
       of putting it in both: 3.bundle.js and 4.bundle.js
      */
     //require('commons/msg-store').name,
     require('./pages/home/home.routing').name,
-    require('./pages/messages/messages.routing').name,
+    require('./pages/messages/messages.routing').name
   ]);
