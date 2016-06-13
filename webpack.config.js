d56a159b01ac114ee72b57028cec1326ffd0d94d
blackendstudios <jeroflat@gmail.com>
2016-06-12 20:41:03 -0500

    feat(webpack.config): resolve angular.min.js to optimize bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
+let path = require('path'),
+    nodeModulesDir = path.join(__dirname, './node_modules');
+
+let deps = [
+   'angular/angular.min.js'
+];
+
 var config = {
-  entry: {
-    app: ['./src/core/bootstrap.js']
-  },
-  output: {
-    path:     __dirname + '/build/',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    root: __dirname + '/src/'
-  },
-  module: {
-    noParse: [],
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'ng-annotate!babel' },
-      { test: /\.html$/, loader: 'raw' }
-    ]
-  }
+   entry: {
+      app: ['./src/core/bootstrap.js']
+   },
+   output: {
+      path: __dirname + '/build/',
+      filename: 'bundle.js'
+   },
+   resolve: {
+      alias: {},
+      root: __dirname + '/src/'
+   },
+   module: {
+      noParse: [],
+      loaders: [
+         {test: /\.js$/, exclude: /node_modules/, loader: 'ng-annotate!babel'},
+         {test: /\.html$/, loader: 'raw'}
+      ]
+   }
 };
+
+deps.forEach(function (dep) {
+   let depPath = path.resolve(nodeModulesDir, dep);
+   config.resolve.alias[dep.split(path.sep)[0]] = depPath;
+   config.module.noParse.push(depPath);
+});
 
 module.exports = config;
