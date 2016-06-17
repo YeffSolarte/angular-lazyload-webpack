7c25de0b81756c7bede586ce2e751fa21acda86e
blackendstudios <jeroflat@gmail.com>
2016-06-16 23:10:53 -0500

    refactor(home): change 'load' method to 'inject' on $onLazyLoad

--- src/pages/home/home.controller.js
+++ src/pages/home/home.controller.js
@@ -3,32 +3,31 @@
 class HomeController {
    constructor($ocLazyLoad, $injector, $q) {
       this.callService = () => {
-         $ocLazyLoad.load({
-            name: (() => {
-               return $q((resolve, reject) => {
-                  require.ensure([], () => {
-                     // load whole module
-                     let module = require('./home.service').default;
-                     $ocLazyLoad.load({name: module.name}).then((promise) => {
-                        console.log('promise');
-                        console.log(promise[0]);
-                        console.log(Array.isArray(promise));
-                        resolve(promise[0]);
-                     });
-                  })
-               });
-            })()
+         $ocLazyLoad.inject({
+            name: $q((resolve, reject) => {
+               require.ensure([], () => {
+                  // load whole module
+                  let module = require('./home.service').default;
+                  $ocLazyLoad.load({name: module.name}).then((promise) => {
+                     resolve(promise[0]);
+                  });
+               })
+            })
          }).then(() => {
             console.log(`$injector.get('HomeService')`);
             console.log($injector.get('HomeService'));
          })
       };
+
+
    }
 
    $onInit() {
       console.log('this');
       console.log(this);
    }
+
+
 }
 
 HomeController.$inject = ['$ocLazyLoad', '$injector', '$q'];
